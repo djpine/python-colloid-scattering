e31f8b0c48346e8043534045b9de60802dd00ced
pine@nyu.edu <pine@nyu.edu>
2020-06-19 08:01:48 -0400

    initial commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="ColloidScattering",
     version="0.0.1",
-    author="David J. Pine, Wenhai Zheng",
+    author="David J. Pine",
     author_email="pine@nyu.edu",
     description="Python package for analysis of single & multiple, static & dynamic light scattering from colloids",
     url="https://github.com/djpine/ColloidScat",
