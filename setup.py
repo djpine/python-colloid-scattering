a3ddda987b6da07f130691fc34bfb99c7811f7e6
pine@nyu.edu <pine@nyu.edu>
2020-06-23 05:34:15 -0400

    corrected packages name

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     author_email="pine@nyu.edu",
     description="Python package for analysis of single & multiple, static & dynamic light scattering from colloids",
     url="https://github.com/djpine/ColloidScat",
-    packages=[colloidscat],
+    packages=[pycollscat],
     install_requires=['numpy', 'scipy']
 )
 
